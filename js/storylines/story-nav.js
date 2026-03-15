38fa68c3550503b0fad1d3a25ed7723adce3a518
Alexander Frank <167268313+MsSuns3t@users.noreply.github.com>
2026-03-15 23:37:45 +0100

    Touch-ups HTML en JS functionaliteit toegevoegd

---
+++ js/storylines/story-nav.js
@@ -0,0 +1,44 @@
+// alle divs van activiteiten in de goede volgorde
+const activities = ['activity-1','activity-2','activity-3'];
+
+// Huidige activiteit bijhouden (standaard/begin index 0)
+let current = 0;
+
+// Selectors voor navigatiebuttons
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+
+function showActivity(index) {
+    // Activiteiten verbergen
+    activities.forEach(function(id) {
+        document.getElementById(id).classList.add('d-none');
+    });
+
+    // Correcte activiteit weergeven
+    document.getElementById(activities[index]).classList.remove('d-none');
+
+    // Linker navigatieknop verbergen op het eerste scherm
+    if (index ===0) {
+        btnPrev.classList.add('d-none');
+    } else {
+        btnPrev.classList.remove('d-none');
+    }
+}
+
+// Eventlisteners en logica voor buttons
+btnNext.addEventListener('click', function() {
+    if (current < activities.length - 1) {
+        current++;
+        showActivity(current);
+    }
+});
+
+btnPrev.addEventListener('click', function(){
+    if (current > 0) {
+        current --;
+        showActivity(current);
+    }
+});
+
+// Initiële pagina laden
+showActivity(0);
