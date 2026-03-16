d9779fb886667e2f52e8d5b3b5d88a99f37eb11f
Alexander Frank <167268313+MsSuns3t@users.noreply.github.com>
2026-03-16 14:04:11 +0100

    Templates gekopieerd
    Templates aan elke storyline toegevoegd

--- js/storylines/storyline3.js
+++ js/storylines/storyline3.js
@@ -1,1 +1,29 @@
 // Alyssa
+// Quizvragen activiteit 4
+const questions = [
+    {
+        question: "Vraag 1 voor verhaal 1?",
+        options: ["Antwoord A", "Antwoord B", "Antwoord C"],
+        correct: 0
+    },
+    {
+        question: "Vraag 2 voor verhaal 1?",
+        options: ["Antwoord A", "Antwoord B", "Antwoord C"],
+        correct: 1
+    },
+    {
+        question: "Vraag 3 voor verhaal 1?",
+        options: ["Antwoord A", "Antwoord B", "Antwoord C"],
+        correct: 2
+    },
+    {
+        question: "Vraag 4 voor verhaal 1?",
+        options: ["Antwoord A", "Antwoord B", "Antwoord C"],
+        correct: 0
+    },
+    {
+        question: "Vraag 5 voor verhaal 1?",
+        options: ["Antwoord A", "Antwoord B", "Antwoord C"],
+        correct: 1
+    }
+];
